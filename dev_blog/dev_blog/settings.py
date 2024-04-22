94cbdcef824f20324d2091fe9376fa42a4495d3c
DK PARK <parkdong1015@naver.com>
2024-04-22 21:54:34 +0900

    '.'

--- dev_blog/dev_blog/settings.py
+++ dev_blog/dev_blog/settings.py
@@ -10,23 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = my_settings.BASE_DIR
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b80+#0#5_74a_&h8!bw4%9_75lir@fb3$#1gn1_t15uwpv(g20'
+SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = my_settings.DEBUG
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = my_settings.ALLOWED_HOSTS
 
 
 # Application definition
--- dev_blog/main_page/views.py
+++ dev_blog/main_page/views.py
@@ -16,17 +16,17 @@ def blog_index(request):
     return render(request, "main_page/index.html", context)
 
 # def categories_list(request, category):
-    posts = Post.objects.filter(categories__name__contains=category).distinct()
-    # paginator = Paginator(posts, 9)
-    # page_number = request.GET.get('page')
-    # page_obj = paginator.get_page(page_number)
-    categories = find_active_categories()
+#     posts = Post.objects.filter(categories__name__contains=category).distinct()
+#     paginator = Paginator(posts, 9)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     categories = find_active_categories()
 
-    context = {
-        # 'page_obj': page_obj,
-        'categories': categories,
-    }
-    return render(request, 'main_page/index.html', context)
+#     context = {
+#         'page_obj': page_obj,
+#         'categories': categories,
+#     }
+#     return render(request, 'main_page/index.html', context)
 
 def find_active_categories():
     categories = [category for category in Category.objects.all() if len(category.posts.all()) > 0]
@@ -36,8 +36,10 @@ def blog_category(request, category):
     posts = Post.objects.filter(
         categories__name__contains=category
     ).order_by("-created_on")
+    categories = find_active_categories()
     context = {
         "category": category,
+        'categories': categories,
         "posts": posts,
     }
     return render(request, "main_page/category.html", context)
